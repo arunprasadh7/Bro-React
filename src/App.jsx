5d3abb6912a67c3fcd605b5e7a8fda2e3110269d
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-03-14 17:55:50 +0530

    1 Basic Folder structure and components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./header";
+import Footer from "./footer";
+import Food from "./food";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Food></Food>
+      <Food></Food>
+      <Food></Food>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
